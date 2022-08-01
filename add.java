d112119b7ca6ab5a6cba92a40b33685cf1579eec
RAM KUMAR <43318753+ramkumarpatwa@users.noreply.github.com>
2022-08-01 21:29:47 +0530

    added Ram

--- add.java
+++ add.java
@@ -3,8 +3,11 @@ public class add {
         int a=20;
         int b=30;
         System.out.println(a+b);
+<<<<<<< HEAD
         System.out.println("Hello");
         System.out.println("Sushma");
         System.out.println("Ram");
+=======
+>>>>>>> parent of ee79c4d (added hello)
     }
 }
